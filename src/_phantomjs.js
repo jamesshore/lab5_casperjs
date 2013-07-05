9c61864d8a3edd31565fea32f3acd38543b072a7
James Shore <jshore@jamesshore.com>
2013-07-05 11:51:09 -0700

    Added user interaction test

--- src/_phantomjs.js
+++ src/_phantomjs.js
@@ -17,21 +17,26 @@
 
 		try {
 			runTest(inBrowserTest);
+			runTest(userInteractionTest);
 			phantom.exit(0);
 		}
 		catch(err) {
-			die("Exception from PhantomJS: " + err);
+			die("Exception from PhantomJS: " + err.stack);
 		}
 	});
 
 	function inBrowserTest() {
 		return page.evaluate(function() {
 			try {
+				// Get the DOM elements
 				var textField = document.getElementById("textField");
 				var validateButton = document.getElementById("validate");
 
+				// Click the validate button
 				validateButton.click();
-				var actual = textField.getAttribute("class");
+
+				// Check the CSS class
+				var actual = textField.className;
 				var expected = example.REQUIRED_FIELD_CLASS;
 				if (actual !== expected) return "textField class expected " + expected + " but was " + actual;
 				else return null;
@@ -39,6 +44,30 @@
 			catch(err) {
 				return "Exception in PhantomJS browser code";
 			}
+		});
+	}
+
+	function userInteractionTest() {
+		// Get validate button location
+		var location = page.evaluate(function() {
+			var validateButton = document.getElementById("validate");
+			var bounds = validateButton.getBoundingClientRect();
+			return {
+				x: bounds.left + document.body.scrollLeft,
+				y: bounds.top + document.body.scrollTop
+			};
+		});
+
+		// Click the validate button
+		page.sendEvent("click", location.x, location.y);
+
+		// Check the CSS class
+		return page.evaluate(function() {
+			var textField = document.getElementById("textField");
+			var actual = textField.className;
+			var expected = example.REQUIRED_FIELD_CLASS;
+			if (actual !== expected) return "textField class expected " + expected + " but was " + actual;
+			else return null;
 		});
 	}
 

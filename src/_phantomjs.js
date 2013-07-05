5bb36f5ec6a4270ec5ef42ac9bedaff726f66e88
James Shore <jshore@jamesshore.com>
2013-07-05 11:22:58 -0700

    Abstracted PhantomJS test running

--- src/_phantomjs.js
+++ src/_phantomjs.js
@@ -16,46 +16,35 @@
 		if (success !== "success") die("PhantomJS could not load " + URL);
 
 		try {
-			var error = page.evaluate(inBrowser);
-			if (error) die(error);
-			else phantom.exit(0);
+			runTest(inBrowserTest);
+			phantom.exit(0);
 		}
 		catch(err) {
 			die("Exception from PhantomJS: " + err);
 		}
 	});
 
-	function inBrowser() {
-		try {
-			var textField = document.getElementById("textField");
-			var validateButton = document.getElementById("validate");
+	function inBrowserTest() {
+		return page.evaluate(function() {
+			try {
+				var textField = document.getElementById("textField");
+				var validateButton = document.getElementById("validate");
 
-			validateButton.click();
-			var actual = textField.getAttribute("class");
-			var expected = example.REQUIRED_FIELD_CLASS;
-			if (actual !== expected) return "textField class expected " + expected + " but was " + actual;
-			else return null;
+				validateButton.click();
+				var actual = textField.getAttribute("class");
+				var expected = example.REQUIRED_FIELD_CLASS;
+				if (actual !== expected) return "textField class expected " + expected + " but was " + actual;
+				else return null;
+			}
+			catch(err) {
+				return "Exception in PhantomJS browser code";
+			}
+		});
+	}
 
-
-//			console.log("HI");
-//			return null;
-//			var client = require("./client.js");
-//			var HtmlElement = require("./html_element.js");
-//
-//			var drawingArea = new HtmlElement($("#drawingArea"));
-//			drawingArea.doMouseDown(10, 20);
-//			drawingArea.doMouseMove(50, 60);
-//			drawingArea.doMouseUp(50, 60);
-//
-//			var actual = JSON.stringify(client.drawingAreaCanvas.lineSegments());
-//			var expected = JSON.stringify([[ "10", "20", "50", "60" ]]);
-//
-//			if (actual !== expected) return "lines drawn expected " + expected + " but was " + actual;
-//			else return null;
-		}
-		catch(err) {
-			return "Exception in PhantomJS browser code";
-		}
+	function runTest(testFn) {
+		var error = testFn();
+		if (error) die(error);
 	}
 
 	function die(error) {

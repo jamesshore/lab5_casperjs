1ff2e7658c0f97c328e59c98cda37a55efa63548
James Shore <jshore@jamesshore.com>
2013-07-04 19:23:17 -0700

    Stubbed in PhantomJS test code

---
+++ src/_phantomjs.js
@@ -0,0 +1,56 @@
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
+/*global phantom, $ */
+
+(function() {
+	"use strict";
+
+	var page = require("webpage").create();
+
+	page.onConsoleMessage = function(message) {
+		console.log("CONSOLE: " + message);
+	};
+
+	page.open("http://localhost:5000", function(success) {
+		console.log("Exiting... " + success);
+		phantom.exit(0);
+
+//		try {
+//			var error = page.evaluate(inBrowser);
+//			if (error) {
+//				console.log(error);
+//				phantom.exit(1);
+//			}
+//			else {
+//				phantom.exit(0);
+//			}
+//		}
+//		catch(err) {
+//			console.log("Exception in PhantomJS code");
+//			phantom.exit(1);
+//		}
+	});
+
+	function inBrowser() {
+		try {
+			console.log("HI");
+			return null;
+//			var client = require("./client.js");
+//			var HtmlElement = require("./html_element.js");
+//
+//			var drawingArea = new HtmlElement($("#drawingArea"));
+//			drawingArea.doMouseDown(10, 20);
+//			drawingArea.doMouseMove(50, 60);
+//			drawingArea.doMouseUp(50, 60);
+//
+//			var actual = JSON.stringify(client.drawingAreaCanvas.lineSegments());
+//			var expected = JSON.stringify([[ "10", "20", "50", "60" ]]);
+//
+//			if (actual !== expected) return "lines drawn expected " + expected + " but was " + actual;
+//			else return null;
+		}
+		catch(err) {
+			return "Exception in PhantomJS browser code";
+		}
+	}
+
+}());
